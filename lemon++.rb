2a0c0d86ce65998237834a6a5639039bc5c00e1e
Kelvin Sherlock <ksherlock@gmail.com>
2016-03-22 13:02:18 -0400

    updated home-brew recipe.

--- lemon++.rb
+++ lemon++.rb
@@ -2,16 +2,14 @@
 class Lemonxx < Formula
   desc "lemon parser generator with improved c++ support"
   homepage "https://github.com/ksherlock/lemon--/"
-  url "https://github.com/ksherlock/lemon--/archive/2016-02-05.tar.gz"
-  sha256 "8b6f19f22772c3df832674646df31cb8f6819b05c16146894bd63a51d9c251f9"
+  url "https://github.com/ksherlock/lemon--/archive/2016-03-22.tar.gz"
+  sha256 "40ba339defe6bd334abd4fb655f7d9d8b57adc5f0737aad647de0470927a81d2"
 
   def install
     (share/"lemon").install "lempar.c"
     (share/"lemon").install "lempar.cpp"
 
-    inreplace "lemon.c", / = pathsearch\([^)]*\);/, " = \"#{share}/lemon/lempar\" FILE_EXTENSION;"
-
-    system "make"
+    system "make HOMEBREW_TEMPLATE_PATH=#{share}/lemon/"
     bin.install "lemon"
     bin.install "lemon++"
   end
